67a21ec7c6e6589a63f41f8f96ba3bae3d4f0368
kimseohyeon1533 <kimseohyeon1533@gmail.com>
2026-04-23 11:57:53 +0900

    :art: Design: 버튼 위치변경 및 사진 추가

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import logUrl from "../../assets/images/kream_image.png"
+import homeUrl from "../../assets/icons/home_icon.png"
+import {useLocation, useNavigate} from "react-router-dom";
+
+const LogoImage = styled.img`
+    width: 166px;
+    height: 141px;
+`;
+
+const HomeIcon = styled.img`
+    width: 61px;
+    height: 24px;
+`;
+
+const HeaderContainer = styled.div`
+    padding-right: 160px;
+    padding-left: 160px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const HeaderRight = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 5px;
+`;
+
+const Button = styled.div`
+    color: #6c6c6c;
+    font-size: 13px;
+    font-family: Pretendard;
+    font-weight: 400;
+    margin-top: 9px;
+`;
+
+export default function Header(){
+
+    const {pathname} = useLocation(); //현재 페이지 경로 불러오기
+    const navigate = useNavigate();
+    const buttonName = "상품등록";
+
+    return(
+        <div>
+            <HeaderContainer>
+            <LogoImage src={logUrl}/>
+            <HeaderRight>
+            {pathname === "/" && (
+                <Button onClick={()=>navigate("/add")}>{buttonName}</Button>
+            )}
+            <HomeIcon src={homeUrl}/>
+            </HeaderRight>
+            </HeaderContainer>
+
+        </div>
+    );
+}
